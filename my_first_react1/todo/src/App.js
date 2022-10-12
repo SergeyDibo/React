93c59e228332561dc6c131514168ae08a2114462
Yurev-Sergey <87377996+SergeyDibo@users.noreply.github.com>
2022-10-12 13:26:02 +0300

    Add files via upload

---
+++ my_first_react1/todo/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Greeting from "./components/Greeting";
+import Counter from "./components/Counter";
+
+function App() {
+  return <div>
+    <Greeting />  
+    <Counter /> 
+  </div>
+}
+export default App;
